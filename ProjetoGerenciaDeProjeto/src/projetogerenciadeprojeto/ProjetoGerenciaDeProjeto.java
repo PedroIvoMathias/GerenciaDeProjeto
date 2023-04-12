f2578c211e8870fb1c1d046816ea7952a8a9e3fb
Pedro Ivo <pedroivomathias@gmail.com>
2023-04-12 16:57:30 -0300

    Update ProjetoGerenciaDeProjeto.java

--- ProjetoGerenciaDeProjeto/src/projetogerenciadeprojeto/ProjetoGerenciaDeProjeto.java
+++ ProjetoGerenciaDeProjeto/src/projetogerenciadeprojeto/ProjetoGerenciaDeProjeto.java
@@ -4,8 +4,7 @@ package projetogerenciadeprojeto;
 public class ProjetoGerenciaDeProjeto {
 
     public static void main(String[] args) {
-        Contas c1 = new Contas(1, "Fernando", 1000.0F);
-        Contas c2= new Contas (2, "Cleber", 2000.0F);
+        
     }
     
 }
